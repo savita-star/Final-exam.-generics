a29ecf7bc5a8160bda17326afd2580e14b531c66
savita <savitaranipathak@hotmail.com>
2022-04-25 19:19:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// We use < > to specify Parameter type
+public class Practice<T>
+{
+
+    private T data;
+
+    public T value
+    {
+
+        get
+        {
+            return this.data;
+        }
+        set
+        {
+            this.data = value;
+        }
+    }
+}
+class Test2
+{
+    static void Main(string[] args)
+    {
+        Practice<string> name = new Practice<string>();
+        name.value = "GeeksforGeeks";
+
+        Practice<float> version = new Practice<float>();
+        version.value = 5.0F;
+
+        Console.WriteLine(name.value);
+
+        Console.WriteLine(version.value);
+    }
+}
